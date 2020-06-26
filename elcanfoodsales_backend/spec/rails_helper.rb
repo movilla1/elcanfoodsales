a483db3efefb5d740745c73243ca63dfa08da339
Mario Villarroel <movilla@gmail.com>
2020-06-25 23:55:54 -0300

    Add coverage report

--- elcanfoodsales_backend/spec/rails_helper.rb
+++ elcanfoodsales_backend/spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
 ENV['RAILS_ENV'] ||= 'test'
+SimpleCov.start
 
 require File.expand_path('../config/environment', __dir__)
 
